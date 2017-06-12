bec67040b8ecec25fc0fd06acd2079d65991bbe9
Oskar Stattin <stattin.o@gmail.com>
2017-06-12 11:01:10 +0200

    sub.file

---
+++ scripts/thresholding_heatmap.py
@@ -0,0 +1,36 @@
+from heatmapthresholding import *
+from detectionboundary import normalize_colors, heat_map_save
+from plotting import heat_map_alt
+import os
+
+m1 = 10
+p = 1000
+theta = 0.4
+hc = 1
+alpha = 1
+t_type = 'hc'
+"""
+classification_grid = np.array([30, 30])
+
+
+error_matrix = classification_region(theta, p, classification_grid, m1, hc, alpha, t_type)
+normalize_colors(error_matrix)
+
+x_lim = np.array([0, 1 - theta])
+y_lim = np.array([0, 1])
+
+msg = '_' + t_type + '_theta=' + str(theta) + '_m1=' + str(m1) + '_p=' + str(p)
+#heat_map_save(error_matrix, classification_grid[0], classification_grid[1], msg)
+heat_map_alt(error_matrix, p, x_lim, y_lim, msg)
+#"""
+
+#"""
+
+x_lim = np.array([0, 1 - theta])
+y_lim = np.array([0, 1])
+for fn in os.listdir('../src/'):
+    if fn.endswith('.txt') and fn.startswith('heatmap_data__cs'):
+        print(fn)
+        mat = np.loadtxt('../src/' + fn)
+        heat_map_alt(mat, p, x_lim, y_lim, fn)
+#"""
